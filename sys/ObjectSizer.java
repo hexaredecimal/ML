76579bdf39abfbca42d226d0b8b5aa67785f553d
hexaredecimal <mfanakagama@gmail.com>
2024-07-26 22:43:38 +0200

    sys: Calculating object sizes at runtime

---
+++ sys/ObjectSizer.java
@@ -0,0 +1,13 @@
+import java.lang.instrument.Instrumentation;
+
+public class ObjectSizer {
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static long getObjectSize(Object o) {
+        return instrumentation.getObjectSize(o);
+    }
+}
