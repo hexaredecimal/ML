ec0066212c88944ab41b54d9e2a7b9e93ca4ddb0
hexaredecimal <mfanakagama@gmail.com>
2024-08-30 14:13:51 +0200

    sys: Revamp the system prelude

--- sys/Backend.java
+++ sys/Backend.java
@@ -1,3 +1,18 @@
+import java.lang.instrument.Instrumentation;
+
+public class ObjectSizer {
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static long getObjectSize(Object o) {
+        return instrumentation.getObjectSize(o);
+    }
+}
+
+
 class SysConv {
   // Convert to Character
     
